f521c4619fc8c54450f405da70e58d06e8544eec
Lucas <lgxm3z@gmail.com>
2022-08-12 13:48:19 -0300

    fix lint and stuff

--- packages/core/src/App.tsx
+++ packages/core/src/App.tsx
@@ -1,22 +1,10 @@
-import React, { useCallback } from "react";
-import {
-  Behavior,
-  Color3,
-  Color4,
-  Engine,
-  HemisphericLight,
-  Mesh,
-  MeshBuilder,
-  Node,
-  Nullable,
-  Observer,
-  Scene,
-  StandardMaterial,
-  UniversalCamera,
-  Vector3,
-} from "@babylonjs/core";
-import { View } from "react-native";
-import GLRenderer from "./components/GLRenderer";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, {useCallback} from 'react';
+import {Behavior, Color3, Color4, Engine, HemisphericLight, Mesh, MeshBuilder, Node, Nullable, Observer, Scene, StandardMaterial, UniversalCamera, Vector3} from '@babylonjs/core';
+import {View} from 'react-native';
+import GLRenderer from './components/GLRenderer';
 
 export default function App() {
   const onCreateEngine = useCallback((engine: Engine | undefined) => {
@@ -26,14 +14,14 @@ export default function App() {
 
     scene.clearColor = Color4.FromHexString(`#00CBF1`);
 
-    const camera = new UniversalCamera("camera", new Vector3(0, 3, -5), scene);
+    const camera = new UniversalCamera('camera', new Vector3(0, 3, -5), scene);
     camera.setTarget(Vector3.Zero());
 
-    var light = new HemisphericLight("HemiLight", new Vector3(0, 9, -5), scene);
+    const light = new HemisphericLight('HemiLight', new Vector3(0, 9, -5), scene);
 
-    const box = MeshBuilder.CreateBox("Cube", { size: 1 });
-    const boxMaterial = new StandardMaterial("CubeMaterial", scene);
-    boxMaterial.diffuseColor = Color3.FromHexString("#0081fe");
+    const box = MeshBuilder.CreateBox('Cube', {size: 1});
+    const boxMaterial = new StandardMaterial('CubeMaterial', scene);
+    boxMaterial.diffuseColor = Color3.FromHexString('#0081fe');
     box.material = boxMaterial;
 
     box.position.set(0, 0, 0);
@@ -42,14 +30,14 @@ export default function App() {
     const RotationBehaviour: Behavior<Node> & {
       observer?: Nullable<Observer<any>>;
     } = {
-      name: "RotationBehaviour",
+      name: 'RotationBehaviour',
       init: function (): void {},
       attach: function (target: Node): void {
         RotationBehaviour.observer = scene.onBeforeRenderObservable.add(() => {
           (target as Mesh).addRotation(0, engine.getDeltaTime() * 0.001, 0);
         });
       },
-      detach: function (): void {},
+      detach: function (): void {}
     };
 
     box.addBehavior(RotationBehaviour);
@@ -69,10 +57,10 @@ export default function App() {
     <View
       style={{
         flex: 1,
-        backgroundColor: "#000000",
-        position: "absolute",
-        width: "100%",
-        height: "100%",
+        backgroundColor: '#000000',
+        position: 'absolute',
+        width: '100%',
+        height: '100%'
       }}
     >
       <GLRenderer onCreateEngine={onCreateEngine} />
